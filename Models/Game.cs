bf63cfd455e40874678ea6b98251f5dd1491caf3
LWTthomasparcher <s-thomas.parcher@lwtech.edu>
2019-11-03 15:10:31 -0800

    Tenative Game and User Models

---
+++ Models/Game.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroupF.Models
+{
+    public class Game
+    {
+        public int GameId { get; set; }
+        public string Name { get; set; }
+        public DateTime PurchaseDate { get; set; }
+        public float TimePlayed { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroupF.Models
+{
+    public class User
+    {
+        public int SteamId { get; set; }
+        public string Username { get; set; }
+    }
+}
